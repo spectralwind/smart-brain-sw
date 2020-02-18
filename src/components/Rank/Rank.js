2b26ce4cfa02bcd0d507c22ff605d525a1ff985e
Tim Petrov <timofey1992@gmail.com>
2020-02-19 02:02:39 +0300

    deployed

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = ({name, entries}) => {
+  return (
+    <div>
+      <div className='white f3 b'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1 b'>
+        {entries}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
